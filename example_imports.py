f1e52f8af74b7286ecf9e3ddbae80eb692c241cf
Evan Hansen <ev.hansen@umbc.edu>
2022-08-03 15:11:12 -0400

    Addeded up to date .env operations to example file

--- example_imports.py
+++ example_imports.py
@@ -13,26 +13,22 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # CDF Operations
+import cdflib
 from spacepy import pycdf
-from pyspedas.rbsp import hope
+import xarray as xr
+from xarray.core.dataset import Dataset as XarrDataset
+from pyspedas.rbsp import hope, efw
 
-# Calculations and array/list ops
-import numpy
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 from tqdm import tqdm
 
-
-# Find OS-specific CDF_LIB path in .env file. Make sure you
+# Find OS-specific paths from .env file. Make sure you
 # set whichever is patform-relevant to you
 if (platform.system() == "Windows"):
     HOME_DIR = os.environ["WIN_HOME_DIR"]
-    CDF_LIB = os.environ["WIN_CDF_LIB"]
-    os.environ["CDF_LIB"] = CDF_LIB
 elif (platform.system() == "Linux"):
     HOME_DIR = os.environ["LINUX_HOME_DIR"]
-    CDF_LIB = os.environ["LINUX_CDF_LIB"]
-    os.environ["CDF_LIB"] = CDF_LIB
 elif (platform.system() == "Darwin"):
     HOME_DIR = os.environ["MAC_HOME_DIR"]
-    CDF_LIB = os.environ["MAC_CDF_LIB"]
-    os.environ["CDF_LIB"] = CDF_LIB
